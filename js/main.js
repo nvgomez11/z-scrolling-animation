71dc495711e85241e72cdf9ca6dbd16d970aa693
Nelson Gomez Alvarado <nelsongomezalvarado@Nelsons-MacBook-Pro.local>
2021-03-11 22:42:12 -0600

    indicators working properly

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 $( document ).ready(function() {
     console.log("AFTER READY");
 
+    //SET FARME HEIGHT
+    var amountSection = ($(".section").length * 100 ) + 'vh';
+    console.log("amount of sections", amountSection);
+    $(".frame").css('height', amountSection)
 
+    //SET SECTIONS PERSPECTIVE AND OPACITY
     var perspective = '500px'
     var sectionDiference = 500 
     $('.section').each(function(i) {
@@ -15,7 +20,15 @@ $( document ).ready(function() {
         }
     });
 
-    //TRIGGER HEIGHT DINAMICALLY
+    
+    //INSERT TRIGGERS DYNAMICALLY
+    $('.section').each(function(i) {
+        var trigger = '<div class="trigger"></div>';
+        $(".frame").append(trigger);
+    });
+
+
+    //TRIGGER POSITION DINAMICALLY
     var startingTriggerTop = 100
     $('.trigger').each(function(i) {
         if(i==0){
@@ -32,143 +45,250 @@ $( document ).ready(function() {
     });
 
 
-    //SET INDICATOR HREF DINAMICALLY
-    $('.indicator').each(function(i) {
-        $(this).attr('href', '#trigger'+(i.toString()));
+    //for frame ponter proposes
+    $("pin-spacer").css('margin','1px');
+    $("pin-spacer").css('padding-bottom','0px');
+
+
+    //INSERT INDICATORS DYNAMICALLY
+    $('.section').each(function(i) {
+        var trigger = "#trigger" + (i.toString());
+        var indicator = '<a class="indicator" onclick="testing(\''+ trigger +'\')"><div></div></a>'
+        console.log("INDICATOR", indicator);
+        $(".indicators-container__controls").append(indicator);
     });
 
+    
 
-});
-
-
-//SET TRIGGER-PIN HEIGHT
-var sections = document.querySelectorAll('.section');
-var sectionsLength = ((sections.length * 100)-80 ).toString() + 'vh';
-console.log("sections length", sectionsLength)
-$(".trigger-pin").height(sectionsLength);
-
-
-//SET PERSPECTIVE AND Z VALUE OF EACH SECTION
-console.log("GOOGOGOGOGOG");
-
-//animation
-var frames = document.querySelectorAll('.section');
-var framesLength = frames.length
-var arrayFrames = Array.prototype.slice.call(frames);
-
-console.log("array frames", arrayFrames);
-
-//Fill Triggers 
-var triggers = document.querySelectorAll(".trigger");
-console.log("Triggers are", triggers)
-
-
-//Fill Z Values
-var originalZValues = [];
-for(var j=0; j<framesLength; j++){
-    if (j==0){
-        originalZValues.push(0)
-    }else{
-        originalZValues.push(500 * -j)
-    }
-}
-console.log("Z values are", originalZValues)
-
-
-var opacityValues = [0, 1];
-console.log("opacity values are", opacityValues);
-
-
-
-gsap.registerPlugin(ScrollTrigger);
-
-for(var i = 0; i < triggers.length -1; i++){
-    var currentTrigger = triggers[i];
-    var initialZvalues = []
-    var finalZvalues = []
-    //fill initial z values
-    for(var w=0; w<originalZValues.length; w++){
-        initialZvalues.push( originalZValues[w] + (500 * i ));
-    }
-    console.log("initial z values", initialZvalues);
-    //fill final z values
-    for(var w=0; w<originalZValues.length; w++){
-        finalZvalues.push( originalZValues[w] + (500 * (i + 1)));
-    }
-    console.log("final z values", finalZvalues);
-
-    var tl = gsap.timeline({
-        scrollTrigger: {
-            trigger: currentTrigger,
-            start: 'top 10px',
-            end: 'bottom 0px',
-            scrub: 2,
-            // onEnter onLeave onEnterBack onLeaveBack
-            toggleActions: "play none reverse none",
-            markers: {
-                fontSize: '2rem'
+    if(!$(".frame").hasClass('frame--edit')){
+        //SET TRIGGER-PIN HEIGHT
+        var sections = document.querySelectorAll('.section');
+        var sectionsLength = ((sections.length * 100)-120 ).toString() + 'vh';
+        console.log("sections length", sectionsLength)
+        $(".trigger-pin").height(sectionsLength);
+    
+    
+        //SET PERSPECTIVE AND Z VALUE OF EACH SECTION
+        console.log("GOOGOGOGOGOG");
+    
+        //animation
+        var frames = document.querySelectorAll('.section');
+        var framesLength = frames.length
+        var arrayFrames = Array.prototype.slice.call(frames);
+    
+        console.log("array frames", arrayFrames);
+    
+        //Fill Triggers 
+        var triggers = document.querySelectorAll(".trigger");
+        console.log("Triggers are", triggers)
+    
+    
+        //Fill Z Values
+        var originalZValues = [];
+        for(var j=0; j<framesLength; j++){
+            if (j==0){
+                originalZValues.push(0)
+            }else{
+                originalZValues.push(500 * -j)
             }
         }
-    });
-
-    for(var j = 0; j < originalZValues.length; j++){
-
-        var initialValue = initialZvalues[j];
-        var finalValue = finalZvalues[j];
-
-        var initialOpacity = undefined; 
-        var finalOpacity = undefined;
-
-        if (finalValue === 500){
-            initialOpacity = 1
-            finalOpacity = 0
-        }
-        if(finalValue === 0){
-            initialOpacity = 0
-            finalOpacity = 1
+        console.log("Z values are", originalZValues)
+    
+    
+        var opacityValues = [0, 1];
+        console.log("opacity values are", opacityValues);
+    
+    
+    
+        gsap.registerPlugin(ScrollTrigger);
+    
+        for(var i = 0; i < triggers.length -1; i++){
+            var currentTrigger = triggers[i];
+            var initialZvalues = []
+            var finalZvalues = []
+            //fill initial z values
+            for(var w=0; w<originalZValues.length; w++){
+                initialZvalues.push( originalZValues[w] + (500 * i ));
+            }
+            console.log("initial z values", initialZvalues);
+            //fill final z values
+            for(var w=0; w<originalZValues.length; w++){
+                finalZvalues.push( originalZValues[w] + (500 * (i + 1)));
+            }
+            console.log("final z values", finalZvalues);
+    
+            var tl = gsap.timeline({
+                onComplete: function(){ console.log("Hola")},
+                scrollTrigger: {
+                    trigger: currentTrigger,
+                    start: 'top 10px',
+                    end: 'bottom 0px',
+                    scrub: 2,
+                    // onEnter onLeave onEnterBack onLeaveBack
+                    toggleActions: "play none reverse none",
+                    markers: {
+                        fontSize: '2rem'
+                    }
+                }
+            });
+    
+            for(var j = 0; j < originalZValues.length; j++){
+    
+                var initialValue = initialZvalues[j];
+                var finalValue = finalZvalues[j];
+    
+                var initialOpacity = undefined; 
+                var finalOpacity = undefined;
+    
+                if (finalValue === 500){
+                    initialOpacity = 1
+                    finalOpacity = 0
+                }
+                if(finalValue === 0){
+                    initialOpacity = 0
+                    finalOpacity = 1
+                }
+            
+                tl.fromTo(arrayFrames[j], {
+                    z: initialValue,
+                    immediateRender: false,
+                    duration: 3,
+                    opacity: initialOpacity
+                }, {
+                    z: finalValue,
+                    immediateRender: false,
+                    duration: 3,
+                    opacity: finalOpacity
+                }, 0);
+                console.log("Creado asi ----")
+                console.log("Frame", arrayFrames[j]);
+                console.log("initial Z value", initialValue)
+                console.log("final  Z value", finalValue)
+                console.log("opacidad inicial", initialOpacity);
+                console.log("opacidad final", finalOpacity);
+                console.log("Trigger", currentTrigger);
+                console.log("end -----")        
+            }
+            console.log("NEXT TRIGGER ---------------");
         }
     
-        tl.fromTo(arrayFrames[j], {
-            z: initialValue,
-            //immediateRender: false,
-            duration: 3,
-            opacity: initialOpacity
-        }, {
-            z: finalValue,
-            immediateRender: false,
-            duration: 3,
-            opacity: finalOpacity
-        }, 0);
-        console.log("Creado asi ----")
-        console.log("Frame", arrayFrames[j]);
-        console.log("initial Z value", initialValue)
-        console.log("final  Z value", finalValue)
-        console.log("opacidad inicial", initialOpacity);
-        console.log("opacidad final", finalOpacity);
-        console.log("Trigger", currentTrigger);
-        console.log("end -----")        
+    
+    
+    
+        //Frame container height dinamically set depending on the amount of frames
+        // var framesContainerHeight = (100 * framesLength).toString() + 'vh';
+        // $('.frame').height(framesContainerHeight);
+    
+    
+        // $('.section').each(function(i) {
+        //     let st = ScrollTrigger.create({
+        //         trigger: ".trigger-pin",
+        //         pin: $(this),
+        //         start: "top 6%",
+        //         end: "bottom 0.5%",
+        //         markers: {
+        //             startColor: "orange", endColor: "orange", fontSize: "16px"
+        //         }
+        //     });
+        // });
+    
+    
+        let st = ScrollTrigger.create({
+            trigger: ".trigger-pin",
+            pin: ".sections-container",
+            start: "top 6%",
+            end: "bottom 0.5%",
+            markers: {
+                startColor: "orange", endColor: "orange", fontSize: "16px"
+            }
+        });
+    
+    
+    
+    
+    
+        // window.addEventListener('scroll',(event) => {
+        //     var y = window.scrollY
+    
+        //     console.log(y);
+    
+        //     // $('.trigger').each(function(i) {
+        //     //     var trigger = $(this);
+        //     //     var position = trigger.offset();
+        //     //     console.log("Trigger #", i);
+        //     //     console.log("Has position", position);
+        //     // });
+        // });
+    
+    
+    
+        // function testing(trigger){
+        //     console.log("jajajaja");
+        //     var triggerElement = $(trigger);
+        //     var bodyElement = $("body"); 
+        //     console.log("trigger element is", triggerElement);
+        //     console.log($(trigger).offset());
+    
+        //     //var position = 0
+        //     // var position = trigger1.offset().top  
+        //     //     - documentt.offset().top  
+        //     //     + documentt.scrollTop(); 
+        //     // if(trigger=="#trigger0"){
+        //     //     position = 1125
+        //     // }
+        //     // if(trigger=="#trigger1"){
+        //     //     position = 1961
+        //     // }
+        //     // $([document.documentElement, document.body]).animate({
+        //     //     scrollTop: 0
+        //     // }, 2000);
+    
+        //     // $('html, body').animate({
+        //     //     scrollTop: 100,
+        //     // }, 1000);
+        //     //window.scrollBy(0, 1125);
+        //     //Window.scroll(0, 1125);
+            
+        // }
+    
+    
+    
+        // var documentt = $("body"); 
+        // var trigger1 = $("#trigger1"); 
+        // console.log("q pasa");
+        // console.log("trigger 1 offset", trigger1);
+    
+        // // Calculating new position of scrollbar 
+        // var position = trigger1.offset().top  
+        //         - documentt.offset().top  
+        //         + documentt.scrollTop(); 
+        // console.log("position is", position);
+        // // Setting the value of scrollbar 
+    
+        // document.scrollTop(position);
     }
-    console.log(tl.progress());
-    console.log("NEXT TRIGGER ---------------");
-}
 
-
-
-
-//Frame container height dinamically set depending on the amount of frames
-// var framesContainerHeight = (100 * framesLength).toString() + 'vh';
-// $('.frame').height(framesContainerHeight);
-
-
-let st = ScrollTrigger.create({
-    trigger: ".trigger-pin",
-    pin: ".frame",
-    start: "top 5%",
-    end: "bottom 1%",
-    markers: {
-        startColor: "orange", endColor: "orange", fontSize: "16px"
-    }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
